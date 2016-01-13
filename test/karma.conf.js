43d4893d72909044838753e82bdbe0724646d1d6
Mariana Ramos Franco <ramos.franco@gmail.com>
2016-01-13 12:13:28 +0100

    Some tests for the tictactoe component

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -40,8 +40,14 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['mocha'],
 
+    plugins: [
+      'karma-browserify',
+      'karma-mocha',
+      'karma-mocha-reporter',
+      'karma-chrome-launcher'
+    ],
 
     // web server port
     port: 9876,
